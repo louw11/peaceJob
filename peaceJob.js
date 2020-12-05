a733b2a315edbeb8cddd8b7f5683b2af0ffe82c4
Motshabaesi_Louw <motshabaesi@gmail.com>
2020-12-05 13:52:07 +0200

    My very first git deposit of a project called PeaceJob

---
+++ peaceJob.js
@@ -0,0 +1,13 @@
+//Alert message to greet users
+function welcome(){
+  alert("Welcome To Peace Job where you can find freelancers to hire for a small task or push your side hustle!!!")
+}
+//Alert message to tell users to click for more information
+function instruction(){
+  alert("Click on the button to see what is available")
+}
+//Alert message to tell buyers to click for more information
+function buyerInstruct(){
+  alert("Click on the button to see what is available")
+}
+//
